03966ba8d8a54392eb1be62813b90e7e63ba8ee7
Geoff Goetz <geoff.goetz503@gmail.com>
2020-02-14 11:39:50 -0800

    Add additional functionality code.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { DoctorFinder } from './doctor-service';
 
+
+
+
+function showDoctors(array) {
+  for (let i=0; i<array.length; i++) {
+    $("#doctorOutput").append("<li>" + array[i].name + "</li>");
+  }
+}
+
+
 $(document).ready(function() {
   $("form#complaint").submit(function(event) {
     event.preventDefault();
@@ -11,16 +21,14 @@ $(document).ready(function() {
     
     (async () => {
       let chiefComplaint = new DoctorFinder();
-      const complaintResponse = await chiefComplaint.findDoctorByCC(complaint);
+      const complaintResponse = await chiefComplaint.findDoctorByCC(complaintInput);
       
       if (complaintResponse.length === 0) {
         $("#output").text("Unfortunately there is nothing available in your area.");
       } else if (complaintResponse === false) {
         $("#output").text("It seems something has gone wrong, please wait a moment and try again.");
-      }
-
-      
-
-
+      } else 
+      showDoctors(complaintResponse)
+    })();
   });
 });
