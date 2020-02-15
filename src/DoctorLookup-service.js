c6af5cbcd1d3a37722d41595f60beb16ff420e19
Geoff Goetz <geoff.goetz503@gmail.com>
2020-02-14 16:30:53 -0800

    Remove console logs

--- src/DoctorLookup-service.js
+++ src/DoctorLookup-service.js
@@ -15,8 +15,6 @@ export class DoctorFinder {
   async findDoctorByCC(complaint) {
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${complaint}&location=or-portland&sort=distance-asc&skip=0&user_key=${process.env.API_KEY}`);
-      console.log(response)
-      console.log(complaint)
       if (response.status != 200 || response.ok != true) {
         return false;
       } else {
