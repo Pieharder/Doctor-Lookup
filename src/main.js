b36c3f5fc75b729f41660d1de7b25e571da6f730
Geoff Goetz <geoff.goetz503@gmail.com>
2020-02-14 16:31:24 -0800

    Continue troubleshooting displaying API call results.

--- src/main.js
+++ src/main.js
@@ -7,11 +7,12 @@ import { DoctorFinder } from './DoctorLookup-service';
 
 
 
-function showDoctors(array) {
-  for (let i=0; i<array.length; i++) {
-    $("ul#doctorOutput").append("<li>" + array[i].profile + "</li>");
-    console.log(array)
-  }
+function showDoctors(compResponse) {
+  if (compResponse)
+    for (let i=0; i<compResponse.data.length; i++) {
+      $("ul#doctorOutput").append("<li>" + compResponse[i] + "</li>");
+      console.log(compResponse);
+    }
 }
 
 
@@ -24,19 +25,14 @@ $(document).ready(function() {
     (async () => {
       let chiefComplaint = new DoctorFinder();
       let complaintResponse = await chiefComplaint.findDoctorByCC(complaintInput);
-      getElements(chiefComplaint);
-      console.log(complaintInput);
       console.log(complaintResponse);
-    })();
-
-
-    const getElements = function(complaintResponse) {
+      console.log(complaintInput);
       if (complaintResponse.length === 0) {
         $("#output").text("Unfortunately there is nothing available in your area.");
       } else if (complaintResponse === false) {
         $("#output").text("It seems something has gone wrong, please wait a moment and try again.");
       } else 
         showDoctors(complaintResponse);
-    };
+    })();
   });
 });
