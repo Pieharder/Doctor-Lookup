ac778d0a0bb5347141c42485d3c78fae358c0075
Geoff Goetz <geoff.goetz503@gmail.com>
2020-02-23 20:00:54 -0800

    Remove irrelevant code.

--- src/DoctorLookup-service.js
+++ src/DoctorLookup-service.js
@@ -1,17 +1,4 @@
 export class DoctorFinder {
-  // async findDoctorByName(name) {
-  //   try {
-  //     let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
-  //     if (response.status != 200 || response.ok != true) {
-  //       return false;
-  //     } else {
-  //       let jsonifiedResponse = await response.json();
-  //       return jsonifiedResponse;
-  //     }
-  //   } catch(error) {
-  //     return false;
-  //   }
-  // }
   async findDoctorByCC(complaint) {
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${complaint}&location=or-portland&sort=distance-asc&skip=0&user_key=${process.env.API_KEY}`);
